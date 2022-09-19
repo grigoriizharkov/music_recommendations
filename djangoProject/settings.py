5a6cf67f3a5e7de7844a8bca39a1b884c7da542d
grigorii <grigoriial54@gmail.com>
2022-09-19 20:23:23 +0300

    WhiteNoise settings updated

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -122,7 +122,7 @@ STATICFILES_DIRS = [
    os.path.join(BASE_DIR, "static"),
 ]
 STATIC_ROOT = BASE_DIR/'staticfiles'
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
